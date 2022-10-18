00359fc3b93a1140beae427d93ffe9b0c73c6150
avocad06 <miggo2704@gmail.com>
2022-10-18 15:27:46 +0900

    add: base.html

--- curd_comments/settings.py
+++ curd_comments/settings.py
@@ -33,7 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'articles',
     'django_bootstrap5',
-    'django_extentions',
+    'django_extensions',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
