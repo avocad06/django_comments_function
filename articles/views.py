1da76222a2c4ac08ac3626d82d13075cfb36bf73
avocad06 <miggo2704@gmail.com>
2022-10-18 17:02:24 +0900

    function: articles_comment-create

--- articles/views.py
+++ articles/views.py
@@ -33,3 +33,12 @@ def create(request):
         "forms" : forms,
     }
     return render(request, 'articles/create.html', context)
+
+def comment_create(request, article_pk):
+    article = Article.objects.get(pk=article_pk)
+    comment_form = NewCommentForm(request.POST)
+    if comment_form.is_valid():
+        comment = comment_form.save(commit=False)
+        comment.article = article
+        comment.save()
+    return redirect('articles:detail', article.pk)
