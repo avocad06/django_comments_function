86c9c7a511e7dfeca5f426e8f86a5a727d3a3bb3
avocad06 <miggo2704@gmail.com>
2022-10-20 15:49:55 +0900

    function: accounts-signout

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path("login/", views.login, name="login"),
     path("logout/", views.logout, name="logout"),
     path("<int:user_pk>", views.detail, name="detail"),
+    path("signout/", views.signout, name="signout"),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth import get_user_model
 from django.contrib.auth import login as auth_login
 from django.contrib.auth import logout as auth_logout
 from django.http import HttpResponseForbidden
+from .forms import SignoutForm
 
 # Create your views here.
 def login(request):
@@ -31,3 +32,27 @@ def logout(request):
 def detail(request, user_pk):
     us = get_user_model().objects.get(pk=user_pk)
     return render(request, "accounts/detail.html", {"us": us})
+
+# 회원가입 함수
+def signout(request):
+    if not request.user.is_authenticated: 
+        if request.method == "POST":
+            # 회원가입 폼이 필요
+            forms = SignoutForm(request.POST)
+            if forms.is_valid():
+                # 객체를 반환
+                user = forms.save()
+                # 모델의 인스턴스 = User클래스로 생성한 객체(인스턴스)
+                # 로그인 함수에 필요한 것은 http객체와 user객체
+                auth_login(request, user)
+                return redirect("articles:index")
+            
+        else:
+            forms = SignoutForm()
+            context = {
+                "forms" : forms,
+            }
+        return render(request, "accounts/signout.html", context)
+    
+    else:
+        return HttpResponseForbidden()
