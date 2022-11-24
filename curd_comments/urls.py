e298461fb9066401c3246b6108df5ee7ccbb0b34
avocad06 <miggo2704@gmail.com>
2022-11-24 23:26:23 +0900

    url 설정

--- curd_comments/urls.py
+++ curd_comments/urls.py
@@ -15,10 +15,19 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+# 이미지 파일 - 사용자가 업로드한 파일의 url를 가져오기 위해서
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('articles/', include("articles.urls")),
     path('accounts/', include("accounts.urls")),
     path('', include("core.urls")),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+# 업로드된 파일의 URL == settings.`MEDIA_URL`
+# 위 URL을 통해 참조하는 `파일의 실제 위치` == settings.MEDIA_`ROOT`
+
+# 사용자가 업로드한 파일이 프로젝트 폴더에 업로드 되지만,
+# 우리가 사용자에게 제공하기 위해서는 업로드된 파일의 경로로부터 만들어진 URL이 필요하다.
